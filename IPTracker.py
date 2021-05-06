c8ec0b0bd2d49de0fe7209fc25b1fdedd79c0069
Hekang Jia <48497574+sflsjhk@users.noreply.github.com>
2021-05-06 01:13:50 -0400

    minor bug fix

--- IPTracker.py
+++ IPTracker.py
@@ -1,6 +1,5 @@
 from collections import defaultdict
 from Node import Node
-import math
 
 """
 Initialization:
--- Node.py
+++ Node.py
@@ -1,4 +1,4 @@
-class Nodes:
+class Node:
     def __init__(self, ip_address):
         self.ip_address = ip_address
         self.count = 0
